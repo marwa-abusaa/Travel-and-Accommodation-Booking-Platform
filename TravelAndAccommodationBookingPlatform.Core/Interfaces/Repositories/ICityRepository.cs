c8265c97f807a10e7215fa1a453e22fbdda80d7f
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:36:16 +0300

    Add ICityRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/ICityRepository.cs
@@ -0,0 +1,16 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface ICityRepository
+{
+    public Task<City> AddCityAsync(City city);
+    public Task<City?> GetCityByIdAsync(int cityId);
+    public Task DeleteCityAsync(int cityId);
+    public Task UpdateCityAsync(City city);
+    public Task<IEnumerable<City>> GetMostVisitedCitiesAsync(int count);
+    public Task<IEnumerable<City>> SearchCityAsync();
+    public Task<PaginatedResult<City>> GetAllCities(PaginationMetadata pagination);
+}
+
