c7196e00486b67e6b1f2a5d452a39632da12b229
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:37:21 +0300

    Add IHotelRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IHotelRepository.cs
@@ -0,0 +1,15 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IHotelRepository
+{
+    public Task<Hotel> AddHotelAsync(Hotel hotel);
+    public Task<Hotel?> GetHotelByIdAsync(int hotelId);
+    public Task DeleteHotelAsync(int hotelId);
+    public Task UpdateHotelAsync(Hotel hotel);
+    public Task<IEnumerable<Hotel>> SearchHotelAsync();
+    public Task<PaginatedResult<City>> GetAllHotels(PaginationMetadata pagination);
+    public Task<IEnumerable<Hotel>> GetRecentVisitedHotelsByUserAsync(int userId);
+}
