f5621f337056d87e2333e29549ef958ddc4c9405
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:27:01 +0300

    feat: add Booking entity with basic properties and PaymentType enum

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Booking.cs
@@ -0,0 +1,20 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class Booking
+{
+    public int BookingId { get; set; }
+    public int UserId { get; set; }
+    public int HotelId { get; set; }
+    public User User { get; set; }
+    public Hotel Hotel { get; set; }
+    public decimal TotalPriceBeforeDiscount { get; set; }
+    public decimal TotalPriceAfterDiscount { get; set; }
+    public string Remarks { get; set; } = string.Empty;
+    public PaymentType PaymentType { get; set; }
+    public DateOnly CheckInDate { get; set; }
+    public DateOnly CheckOutDate { get; set; }
+    public DateOnly BookingDate { get; set; }
+    public ICollection<Room> Rooms { get; set; } = new List<Room>();
+}
