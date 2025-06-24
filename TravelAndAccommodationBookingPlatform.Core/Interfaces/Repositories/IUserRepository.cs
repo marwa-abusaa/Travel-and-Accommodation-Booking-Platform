2e095b74262858fc9e4f2a1f5bc7447259539000
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:35:51 +0300

    Add IUserRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    public Task AddUserAsync(User user);
+    public Task<User?> GetUserByIdAsync(int id);
+    public Task<bool> IsEmailExistsAsync(string email);
+}
