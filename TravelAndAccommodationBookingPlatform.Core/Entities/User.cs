114e2de0bb9288b30c63cd46f36ad8bac3c66c5e
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:12:56 +0300

    feat: add User entity with basic properties

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public int RoleId { get; set; }
+    public Role Role { get; set; }
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+}
