8a2d723e12ba7f2203eca22a3eb3ac94cb91764b
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:56:39 +0300

    feat: add JwtToken record and IJwtTokenGenerator interface for token generation

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Auth/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Auth;
+
+public interface IJwtTokenGenerator
+{
+    public Task<JwtToken> GenerateToken(User inputUser);
+}
