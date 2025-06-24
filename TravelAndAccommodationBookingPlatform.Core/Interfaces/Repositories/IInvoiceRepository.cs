d1bb6773f8adb0e5a8809fcc6f14e03eb2721e3b
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:40:13 +0300

    Add IInvoiceRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IInvoiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IInvoiceRepository
+{
+    public Task<Invoice> AddInvoiceAsync(Invoice invoice);
+    public Task<Invoice?> GetInvoiceByIdAsync(int invoiceId);
+    public Task<IEnumerable<Invoice>> GetUserInvoicesForHotelAsync(int userId, int hotelId);
+    public Task<IEnumerable<Invoice>> GetAllUserInvoicesAsync(int userId);
+}
