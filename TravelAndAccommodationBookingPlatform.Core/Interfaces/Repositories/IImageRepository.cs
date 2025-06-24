238b64f6c6fc9fea70f2be25e1fffb96370cda13
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:38:09 +0300

    Add IImageRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IImageRepository
+{
+    public Task<IEnumerable<Image>> GetHotelImagesAsync(int hotelId);
+    public Task<IEnumerable<Image>> GetRoomImagesAsync(int roomlId);
+    public Task<Image?> GetImageByIdAsync(int imageId);
+    public Task<Image> AddImageAsync(Image image);
+    public Task DeleteImageAsync(int imageId);
+    public Task UpdateImageAsync(Image image);
+}
