913e2addcd7979973b6431672e147c14290b3519
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:39:46 +0300

    Add IDiscountRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IDiscountRepository
+{
+    public Task<Discount> AddDiscountAsync(Discount discount);
+    public Task<Discount?> GetDiscountByIdAsync(int discountId, int roomId);
+    public Task DeleteDiscountAsync(int discountId);
+    public IQueryable GetAllDiscounts();
+}
