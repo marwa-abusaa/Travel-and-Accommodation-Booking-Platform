b27369cc950bad547b77ba3c4051bab284398a83
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:24:35 +0300

    feat: add Review entity with basic properties.

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class Review
+{
+    public int ReviewId { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public int Rate { get; set; }
+    public int UserId { get; set; }
+    public int HotelId { get; set; }
+    public Hotel Hotel { get; set; }
+    public User User { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
