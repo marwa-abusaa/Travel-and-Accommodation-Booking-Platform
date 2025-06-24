5eeea4e66ea6f71e5c5bafa502be6fe917a5331f
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:28:21 +0300

    feat: add Invoice entity with basic properties and PaymentStatus enum

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class Invoice
+{
+    public int InvoiceId { get; set; }
+    public int BookingId { get; set; }
+    public Booking Booking { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+    public string Notes { get; set; } = string.Empty;
+}
