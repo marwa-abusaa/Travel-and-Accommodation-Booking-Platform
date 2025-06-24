716a1e67597e2c635f520b0272e28f932f77f942
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:23:37 +0300

    feat: add Room entity with basic properties and RoomClass enum

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Room.cs
@@ -0,0 +1,19 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class Room
+{
+    public int RoomId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public int HotelId { get; set; }
+    public Hotel Hotel { get; set; }
+    public int AdultCapacity { get; set; }
+    public int ChildrenCapacity { get; set; }
+    public RoomClass RoomClass  { get; set; }
+    public ICollection<Image> Images { get; set; } = new List<Image>();
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    public ICollection<Discount> Discounts { get; set; } = new List<Discount>();
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
