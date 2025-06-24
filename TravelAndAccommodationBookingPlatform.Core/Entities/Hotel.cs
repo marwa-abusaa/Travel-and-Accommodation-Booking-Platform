8e2df4d75906d167aaf82a1609d6fbd82f114c71
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:18:04 +0300

    feat: add Hotel entity with basic properties

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/Hotel.cs
@@ -0,0 +1,20 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class Hotel
+{
+    public int HotelId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int CityId { get; set; }
+    public City City { get; set; }
+    public int OwnerId { get; set; }
+    public Owner Owner { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public int StarRating { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public ICollection<Image> Images { get; set; } = new List<Image>();
+    public ICollection<Room> Rooms { get; set; } = new List<Room>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
