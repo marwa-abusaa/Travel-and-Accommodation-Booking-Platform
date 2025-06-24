d6c6bce0b1e425d88e1c97fbdc9b0ed8c18cb73f
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:39:31 +0300

    Add IOwnerRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IOwnerRepository.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IOwnerRepository
+{
+    public Task<Owner> AddOwnerAsync(Owner owner);
+    public Task<IEnumerable<Owner>> GetHotelOwnersAsync(int hotelId);
+    public Task UpdateOwnerAsync(Owner owner);
+    public Task<Owner> GetOwnerByIdAsync(int ownerId);
+}
