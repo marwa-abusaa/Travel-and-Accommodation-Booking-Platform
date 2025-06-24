9557296f0df022e8e082a099a9a3a113cc20f7c3
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:36:50 +0300

    Add IRoomRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IRoomRepository
+{
+    public Task<Room> AddRoomAsync(Room room);
+    public Task<Room?> GetRoomByIdAsync(int roomId);
+    public Task DeleteRoomAsync(int roomId);
+    public Task UpdateRoomAsync(Room room);
+    public Task<PaginatedResult<Room>> GetAllRoomsAsync(int hotelId, PaginationMetadata pagination);
+    public Task<IEnumerable<Room>> SearchRoomAsync();
+    Task<IEnumerable<Room>> GetRoomsWithRoomClassAsync(RoomClass roomClass);
+}
