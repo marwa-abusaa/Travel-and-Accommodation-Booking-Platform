ea2a72d15efc245911a50ee1ee8b40fb10859cda
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:17:08 +0300

    feat: add City entity with basic properties

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/City.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+
+public class City
+{
+    public int CityId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string PostOffice { get; set; } = string.Empty;
+    public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
