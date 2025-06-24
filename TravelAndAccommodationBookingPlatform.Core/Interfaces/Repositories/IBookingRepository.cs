d9d37aaf553aa2bf0cca94cf7f2301fa1c6eef1d
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:37:46 +0300

    Add IBookingRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IBookingRepository
+{
+    public Task<Booking> AddBookingAsync(Booking booking);
+    public Task<Booking?> GetBookingByIdAsync(int bookingId);
+    public Task DeleteBookingAsync(int bookingId);
+    public Task<IEnumerable<Booking>> GetAllBookingsByUserAsync(int userId);
+    public void PrintPaymentConfirmation(Invoice invoice);
+}
