923a73e4e9b34c8a896d23274630678804d18b13
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:46:25 +0300

    feat: add PaginatedResult and PaginationMetadata for pagination

---
+++ TravelAndAccommodationBookingPlatform.Core/Models/PaginationMetadata.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Models;
+
+public class PaginationMetadata
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
+}
