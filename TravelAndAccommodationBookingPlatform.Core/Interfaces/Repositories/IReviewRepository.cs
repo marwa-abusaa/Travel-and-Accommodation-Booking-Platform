449bb61a4d5e0f09747bf3de96ba93816147b7fa
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:38:58 +0300

    Add IReviewRepository interface for entity operations

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+public interface IReviewRepository
+{
+    public Task<Review> AddReviewAsync(Review review);
+    public Task<Review?> GetReviewByIdAsync(int reviewId, int hotelId);
+    public Task UpdateReviewAsync(Review review);
+    public Task DeleteReviewAsync(int reviewId);
+    Task<int> GetHotelRatingAsync(int hotelId);
+    public Task<IEnumerable<Review>> GetHotelReviewsAsync(int hotelId);
+    public Task<IEnumerable<Review>> GetUserReviewsAsync(int userId, int hotelId);
+}
