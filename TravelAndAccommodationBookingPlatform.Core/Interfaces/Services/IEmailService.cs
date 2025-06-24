ed43a4851a9f29313324832526b1317aa22c089c
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-24 15:51:00 +0300

    Add IEmailService interface for sending emails

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
+
+public interface IEmailService
+{
+    public Task SendPaymentConfirmationEmailAsync(EmailRequest emailRequest);
+}
