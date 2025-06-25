3a918d1a421290f42a12ab85fbe6bfc1df4b190a
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-25 19:58:54 +0300

    refactor: move GetHotelsByOwnerIdAsync method from IOwnerRepository to IHotelRepository

--- TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IOwnerRepository.cs
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IOwnerRepository.cs
@@ -5,7 +5,6 @@ namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
 public interface IOwnerRepository
 {
     public Task<Owner> AddOwnerAsync(Owner owner);
-    public Task<IEnumerable<Hotel>> GetHotelsByOwnerIdAsync(int ownerId);
     public Task UpdateOwnerAsync(Owner owner);
     public Task<Owner?> GetOwnerByIdAsync(int ownerId);
     public Task<Owner?> GetOwnerByHotelIdAsync(int hotelId);
