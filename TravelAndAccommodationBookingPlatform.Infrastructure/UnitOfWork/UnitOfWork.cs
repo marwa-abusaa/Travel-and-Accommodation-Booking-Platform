39bc219020ce8a069f9c675c76014dda437ce6d4
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 22:06:07 +0300

    Add UnitOfWork class implementing IUnitOfWork operations

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.UnitOfWork;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Data;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        var modified = _context.ChangeTracker.Entries<AuditableEntity>()
+                       .Where(e => e.State == EntityState.Modified);
+
+        foreach (var entry in modified)
+            entry.Entity.UpdatedAt = DateTime.UtcNow;
+
+        return await _context.SaveChangesAsync();
+    }
+}
