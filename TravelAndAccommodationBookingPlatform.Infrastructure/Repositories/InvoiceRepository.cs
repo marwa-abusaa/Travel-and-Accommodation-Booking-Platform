26d929335ab2c36918ae4010959c9f5dd3480583
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 20:54:11 +0300

    Add InvoiceRepository implementation for IInvoiceRepository operations

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Repositories/InvoiceRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Data;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Repositories;
+
+public class InvoiceRepository : IInvoiceRepository
+{
+    private readonly AppDbContext _context;
+
+    public InvoiceRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Invoice> AddInvoiceAsync(Invoice invoice)
+    {
+        var newInvoice = await _context.Invoices.AddAsync(invoice);
+        return newInvoice.Entity;
+    }
+
+    public async Task<Invoice?> GetInvoiceByBookingIdAsync(int bookingId)
+    {
+        return await _context.Invoices.FirstOrDefaultAsync(i => i.BookingId == bookingId);
+    }
+
+    public async Task<Invoice?> GetInvoiceByIdAsync(int invoiceId)
+    {
+        return await _context.Invoices.FindAsync(invoiceId);
+    }
+
+    public async Task<IEnumerable<Invoice>> GetUserInvoicesByUserIdAsync(int userId)
+    {
+        return await _context.Invoices
+            .Include(i => i.Booking)
+            .Where(i => i.Booking.UserId == userId)
+            .ToListAsync();
+    }
+
+    public async Task UpdateInvoiceAsync(Invoice invoice)
+    {
+        if (await GetInvoiceByIdAsync(invoice.InvoiceId) != null)
+        {
+            _context.Invoices.Update(invoice);
+        }
+    }
+}
