a9a6f0ae6b8f765adf321fa85001078d5164d940
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 22:49:58 +0300

    refactor: change GenerateToken method to synchronous implementation

--- TravelAndAccommodationBookingPlatform.Core/Interfaces/Auth/IJwtTokenGenerator.cs
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Auth/IJwtTokenGenerator.cs
@@ -5,5 +5,5 @@ namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Auth;
 
 public interface IJwtTokenGenerator
 {
-    public Task<JwtToken> GenerateToken(User inputUser);
+    JwtToken GenerateToken(User inputUser);
 }
