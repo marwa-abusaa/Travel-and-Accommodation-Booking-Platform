a54a4395205697107be9e4aff90eb7c23c5eed37
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 22:52:34 +0300

    Add JwtTokenGenerator class implementing IJwtTokenGenerator for token generation

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Auth/JwtTokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Auth;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Auth;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenGenerator(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public JwtToken GenerateToken(User inputUser)
+    {
+        var keyString = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString!));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, inputUser.UserId.ToString()),
+            new Claim(ClaimTypes.Name, $"{inputUser.FirstName} {inputUser.LastName}"),
+            new Claim(ClaimTypes.Email, inputUser.Email),
+            new Claim(ClaimTypes.Role, inputUser.Role.Name)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtSettings:Issuer"],
+            audience: _configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["JwtSettings:ExpiresInMinutes"])),
+            signingCredentials: credentials
+        );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return new JwtToken(jwt);
+    }
+}
