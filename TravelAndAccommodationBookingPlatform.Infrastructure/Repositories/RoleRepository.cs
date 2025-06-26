63dcc221a72fde0e6245d60310490d0d761d589f
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 20:45:49 +0300

    Add RoleRepository implementation for IRoleRepository operations

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Data;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Repositories;
+
+public class RoleRepository : IRoleRepository
+{
+    private readonly AppDbContext _context;
+
+    public RoleRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Role?> GetRoleByNameAsync(string name)
+    {
+        return await _context.Roles.FirstOrDefaultAsync(r => r.Name == name);
+    }
+}
