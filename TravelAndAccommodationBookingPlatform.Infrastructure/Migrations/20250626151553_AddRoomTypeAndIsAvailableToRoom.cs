d13cf9c0d8bb6fbd83d9a35930143a3f4f9925bf
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-26 20:14:49 +0300

    Add migrations for RoomType and IsAvailable property rename in Room entity

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Migrations/20250626151553_AddRoomTypeAndIsAvailableToRoom.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomTypeAndIsAvailableToRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailble",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PricePerNight",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailble",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerNight",
+                table: "Rooms");
+        }
+    }
+}
