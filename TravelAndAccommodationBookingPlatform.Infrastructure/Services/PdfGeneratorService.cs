a2275ce8a276fdf7254aa9951c25fcc683ba1caa
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-28 19:28:16 +0300

    feat: add PdfGeneratorService implementation for generating PDFs

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Services/PdfGeneratorService.cs
@@ -0,0 +1,32 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Services;
+
+public class PdfGeneratorService : IPdfGeneratorService
+{
+    private readonly IConverter _converter;
+
+    public PdfGeneratorService(IConverter converter)
+    {
+        _converter = converter;
+    }
+    public byte[] GeneratePdfFromHtml(string htmlContent)
+    {
+        var doc = new HtmlToPdfDocument()
+        {
+            GlobalSettings = {
+                PaperSize = PaperKind.A4,
+                Orientation = Orientation.Portrait
+            },
+            Objects = {
+                new ObjectSettings {
+                    HtmlContent = htmlContent
+                }
+            }
+        };
+
+        return _converter.Convert(doc);
+    }
+}
