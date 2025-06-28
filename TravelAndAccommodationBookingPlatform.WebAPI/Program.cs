d07a6d4be2fee8d5410692114743671f79ea18e4
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-28 19:36:52 +0300

    chore: register new services in program

--- TravelAndAccommodationBookingPlatform.WebAPI/Program.cs
+++ TravelAndAccommodationBookingPlatform.WebAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
+using TravelAndAccommodationBookingPlatform.Core.Models;
 using TravelAndAccommodationBookingPlatform.Infrastructure.Data;
+using TravelAndAccommodationBookingPlatform.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,12 @@ if (string.IsNullOrEmpty(connectionString))
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(connectionString));
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+builder.Services.AddScoped<IPdfGeneratorService, PdfGeneratorService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 //builder.Services.AddControllers();
 //builder.Services.AddOpenApi();
