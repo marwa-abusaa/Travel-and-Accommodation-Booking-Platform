09cf220a78f4d2c38609f1d5dba89999d0a887a6
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-28 17:37:32 +0300

    Update properties in EmailRequest and FileAttachment models

--- TravelAndAccommodationBookingPlatform.Core/Models/EmailRequest.cs
+++ TravelAndAccommodationBookingPlatform.Core/Models/EmailRequest.cs
@@ -1,12 +1,9 @@
-﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
-
-namespace TravelAndAccommodationBookingPlatform.Core.Models;
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Models;
 
 public class EmailRequest
 {
-    public string? userEmail { get; set; }
-    public string? SubjectLine { get; set; }
+    public string? ToUserEmail { get; set; }
+    public string? Subject { get; set; }
     public string? MessageBody { get; set; }
-    public Invoice? invoice { get; set; }
     IEnumerable<FileAttachment>? FileAttachment { get; set; }
 }
