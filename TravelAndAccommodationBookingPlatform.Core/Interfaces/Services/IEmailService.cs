0f452f480e93a2ce36b7d93f7729a8936a5c7861
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-28 17:40:02 +0300

    refactor: remove redundant 'public' modifier from interface method

--- TravelAndAccommodationBookingPlatform.Core/Interfaces/Services/IEmailService.cs
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/Services/IEmailService.cs
@@ -4,5 +4,5 @@ namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
 
 public interface IEmailService
 {
-    public Task SendPaymentConfirmationEmailAsync(EmailRequest emailRequest);
+    Task SendPaymentConfirmationEmailAsync(EmailRequest emailRequest);
 }
