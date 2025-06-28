d92e4895cf634463adcc1df04b1f8527b459d9b2
marwa-abusaa <s12028718@stu.najah.edu>
2025-06-28 19:31:56 +0300

    feat: add EmailService implementation and configure SMTP settings

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+using Microsoft.Extensions.Options;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Services;
+
+public class EmailService : IEmailService
+{
+    private readonly SmtpSettings _smtpSettings;
+
+    public EmailService(IOptions<SmtpSettings> smtpSettings)
+    {
+        _smtpSettings = smtpSettings.Value;
+    }
+
+    public async Task SendPaymentConfirmationEmailAsync(EmailRequest emailRequest)
+    {
+        var email = new MimeMessage();
+
+        email.From.Add(MailboxAddress.Parse(_smtpSettings.UserName));
+        email.To.Add(MailboxAddress.Parse(emailRequest.ToUserEmail));
+        email.Subject = emailRequest.Subject;
+
+        var builder = new BodyBuilder
+        {
+            HtmlBody = emailRequest.MessageBody
+        };
+
+        if (emailRequest.FileAttachment != null)
+        {
+            foreach (var attachment in emailRequest.FileAttachment)
+            {
+                builder.Attachments.Add(attachment.FileName, attachment.Content, ContentType.Parse(attachment.ContentType));
+            }
+        }
+
+        email.Body = builder.ToMessageBody();
+
+        using var smtp = new SmtpClient();
+
+        await smtp.ConnectAsync(_smtpSettings.SmtpServer, _smtpSettings.Port, SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(
+            _smtpSettings.UserName,
+            _smtpSettings.Password
+        );
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+    }
+}
