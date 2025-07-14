0d83dbba72ac2acdf613b922554fff80b087af44
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:41:15 +0300

    feat(images): add IImageQueryService interface for image read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/IImageQueryService.cs
@@ -0,0 +1,8 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Images;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface IImageQueryService
+{
+    Task<ImageResponseDto?> GetImageByIdAsync(int imageId);
+}
