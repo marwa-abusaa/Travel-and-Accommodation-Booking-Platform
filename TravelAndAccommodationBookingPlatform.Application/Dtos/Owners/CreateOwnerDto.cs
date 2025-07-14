3c48d249c2d63b30420ffc30fd3345a748de267a
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:47:25 +0300

    feat(owners): add Owner DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Owners/CreateOwnerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.Dtos.Owners;
+
+public class CreateOwnerDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Owners/UpdateOwnerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.Dtos.Owners;
+
+public class UpdateOwnerDto
+{
+    public int OwnerId { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
