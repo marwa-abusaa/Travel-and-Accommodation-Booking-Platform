b5ec429179e590a6e782f7fe569b858ca580f770
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:37:58 +0300

    feat(hotels): add AutoMapper profile for mapping DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/HotelMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Hotels;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class HotelMapper : Profile
+{
+    public HotelMapper()
+    {
+        CreateMap<CreateHotelDto, Hotel>();
+        CreateMap<UpdateHotelDto, Hotel>();
+        CreateMap<Hotel, HotelResponseDto>();
+        CreateMap<Hotel, HotelSearchDto>();
+    }
+}
