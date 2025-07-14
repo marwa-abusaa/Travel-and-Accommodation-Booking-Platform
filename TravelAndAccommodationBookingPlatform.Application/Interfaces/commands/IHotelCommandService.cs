8ad575ea0c69c9618921646f81426778efeb78b4
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:37:32 +0300

    feat(hotels): add IHotelCommandService interface for hotel command operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/commands/IHotelCommandService.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Hotels;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Commands;
+
+public interface IHotelCommandService
+{
+    Task<HotelResponseDto> AddHotelAsync(CreateHotelDto createHotelDto);
+    Task DeleteHotelByIdAsync(int hotelId);
+    Task UpdateHotelAsync(UpdateHotelDto updateHotelDto);
+}
