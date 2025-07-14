9183051837017c2eddd18eeeda2f3ec66ec51cbd
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 02:04:18 +0300

    feat(rooms): add IRoomQueryService interface for room read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/IRoomQueryService.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Rooms;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface IRoomQueryService
+{
+    Task<RoomDetailsDto?> GetRoomByIdAsync(int roomId);
+    Task<PaginatedResult<AdminRoomResponseDto>> SearchRoomsAsync(AdminRoomSearchRequest request);
+    Task<PaginatedResult<RoomResponseDto>> GetAvailableRoomsByHotelIdAsync(int hotelId, PaginationMetadata pagination);
+}
