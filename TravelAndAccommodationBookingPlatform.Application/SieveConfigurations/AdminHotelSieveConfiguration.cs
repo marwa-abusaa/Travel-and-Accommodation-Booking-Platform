67afbdff78dd2e653dee787544eb5888442bcc73
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:39:30 +0300

    feat(sieve): add AdminHotelSieveConfiguration for admin hotel filtering

---
+++ TravelAndAccommodationBookingPlatform.Application/SieveConfigurations/AdminHotelSieveConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Sieve.Services;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Hotels;
+
+namespace TravelAndAccommodationBookingPlatform.Application.SieveConfigurations;
+
+public class AdminHotelSieveConfiguration : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<AdminHotelResponseDto>(x => x.Name).CanFilter().CanSort();
+        mapper.Property<AdminHotelResponseDto>(x => x.StarRating).CanFilter().CanSort();
+        mapper.Property<AdminHotelResponseDto>(x => x.OwnerName).CanFilter().CanSort();
+        mapper.Property<AdminHotelResponseDto>(x => x.RoomCount).CanFilter().CanSort();
+        mapper.Property<AdminHotelResponseDto>(x => x.CreatedAt).CanSort();
+        mapper.Property<AdminHotelResponseDto>(x => x.UpdatedAt).CanSort();
+    }
+}
