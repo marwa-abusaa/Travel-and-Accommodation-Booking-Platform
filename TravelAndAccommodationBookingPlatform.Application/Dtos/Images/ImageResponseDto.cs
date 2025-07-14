4abfd8a4cd451bc87da042bad551113684870cad
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:40:37 +0300

    feat(images): add Image DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Images/ImageResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.Dtos.Images;
+
+public class ImageResponseDto
+{
+    public int ImageId { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Images/UpdateImageDto.cs
@@ -0,0 +1,13 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Dtos.Images;
+
+public class UpdateImageDto
+{
+    public int ImageId { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public ImageType Type { get; set; }
+    public int HotelId { get; set; }
+    public int RoomId { get; set; }
+}
