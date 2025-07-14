b8068cbadda3e849720cea66e9d0f2374a4427f1
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:28:15 +0300

    feat(discounts): add IDiscountQueryService interface for discount read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/IDiscountQueryService.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Discounts;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface IDiscountQueryService
+{
+    Task<DiscountResponseDto?> GetDiscountByIdAsync(int discountId);
+    Task<IEnumerable<DiscountResponseDto>> GetDiscountByRoomIdAsync(int roomId); 
+    Task<PaginatedResult<DiscountResponseDto>> GetDiscountsAsync(PaginationMetadata pagination);
+}
