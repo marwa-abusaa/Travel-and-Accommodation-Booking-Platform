6f76ab20e5acaaeed1b8aed8224a2a8a225a3d99
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:23:57 +0300

    feat(cities): add AutoMapper profile for mapping DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/CityMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Cities;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class CityMapper : Profile
+{
+    public CityMapper()
+    {
+        CreateMap<CreateCityDto, City>();
+        CreateMap<UpdateCityDto, City>();
+        CreateMap<City, AdminCityResponseDto>();
+        CreateMap<City, CityResponseDto>();
+    }   
+}
