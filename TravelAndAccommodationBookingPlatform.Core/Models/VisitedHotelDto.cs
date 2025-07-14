53466242dbfb6d9a6e38dbf4cca583e0d9084148
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-14 23:25:22 +0300

    feat(model): add FeaturedHotelModel and VisitedHotelDto

---
+++ TravelAndAccommodationBookingPlatform.Core/Models/VisitedHotelDto.cs
@@ -0,0 +1,13 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Models;
+
+public class VisitedHotelDto
+{
+    public int HotelId { get; set; }
+    public string HotelName { get; set; } = string.Empty;
+    public string CityName { get; set; } = string.Empty;
+    public int StarRating { get; set; }
+    public Image Thumbnail { get; set; }
+    public decimal TotalPrice { get; set; } 
+}
