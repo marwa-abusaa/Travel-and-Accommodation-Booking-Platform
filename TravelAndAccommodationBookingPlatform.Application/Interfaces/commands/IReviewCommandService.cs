fa119cfec3e41d8f34fc6bcddfb138901c49be46
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:52:57 +0300

    feat(reviews): add IReviewCommandService interface for review command operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/commands/IReviewCommandService.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Reviews;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Commands;
+
+public interface IReviewCommandService
+{
+    Task<ReviewResponseDto> AddReviewAsync(CreateReviewDto createReviewDto);
+    Task UpdateReviewAsync(UpdateReviewDto updateReviewDto);
+    Task DeleteReviewByIdAsync(int reviewId);
+}
