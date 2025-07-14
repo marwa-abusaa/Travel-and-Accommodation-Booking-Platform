165f3730777a9459a543a9157df9e383d97a0e7f
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:54:27 +0300

    feat(reviews): add AutoMapper profile for mapping review DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/ReviewMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Reviews;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class ReviewMapper : Profile
+{
+    public ReviewMapper()
+    {
+        CreateMap<CreateReviewDto, Review>();
+        CreateMap<UpdateReviewDto, Review>();
+        CreateMap<Review, ReviewResponseDto>();
+    }
+}
