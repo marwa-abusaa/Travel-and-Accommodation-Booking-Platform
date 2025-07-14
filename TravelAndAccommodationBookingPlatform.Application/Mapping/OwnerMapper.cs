e3fd60686c7d2857e22699578b8616700aa78557
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:48:44 +0300

    feat(owners): add AutoMapper profile for mapping owner DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/OwnerMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Owners;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class OwnerMapper : Profile
+{
+    public OwnerMapper()
+    {
+        CreateMap<CreateOwnerDto, Owner>();
+        CreateMap<UpdateOwnerDto, Owner>();
+        CreateMap<Owner, OwnerResponseDto>();
+    }
+}
