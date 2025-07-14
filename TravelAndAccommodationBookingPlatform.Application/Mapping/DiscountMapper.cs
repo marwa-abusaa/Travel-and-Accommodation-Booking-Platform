63bdd4edf70371bd153b786c6e470796e6fdfcca
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:28:47 +0300

    feat(discounts): add AutoMapper profile for mapping discount DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/DiscountMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Discounts;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class DiscountMapper : Profile
+{
+    public DiscountMapper()
+    {
+        CreateMap<CreateDiscountDto, Discount>();
+        CreateMap<Discount, DiscountResponseDto>();
+    }
+}
