d5b93eea62d7553fdee591c8d474f5ae515c54f8
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:23:43 +0300

    feat(cities): add ICityQueryService interface for city read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/ICityQueryService.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Cities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface ICityQueryService
+{
+    Task<CityResponseDto?> GetCityByIdAsync(int cityId);
+    Task<IEnumerable<CityResponseDto>> GetMostVisitedCitiesAsync(int count);
+    Task<PaginatedResult<AdminCityResponseDto>> SearchCitiesAsync(AdminCitySearchRequest request);
+}
