b6add5bc0c19598d8f766847f7f44e0f973a1b4c
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:02:14 +0300

    feat(bookings): add AutoMapper profile for mapping Booking DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/BookingMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Bookings;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class BookingMapper : Profile
+{
+    public BookingMapper()
+    {
+        CreateMap<CreateBookingDto, Booking>();
+        CreateMap<Booking, BookingResponseDto>();
+    }
+}
