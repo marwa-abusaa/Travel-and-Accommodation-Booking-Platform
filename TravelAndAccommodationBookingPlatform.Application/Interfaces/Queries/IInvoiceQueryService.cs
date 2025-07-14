ddc042b88d3d5ffb676a47feffa0f7de392dc063
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:44:42 +0300

    feat(invoices): add IInvoiceQueryService interface for invoice read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/IInvoiceQueryService.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Invoices;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface IInvoiceQueryService
+{
+    Task<InvoiceResponseDto?> GetInvoiceByIdAsync(int invoiceId);
+    Task<InvoiceResponseDto?> GetInvoiceByBookingIdAsync(int bookingId);
+    Task<IEnumerable<InvoiceResponseDto>> GetUserInvoicesByUserIdAsync(int userId);
+    Task<byte[]> PrintInvoice(int invoiceId);
+}
