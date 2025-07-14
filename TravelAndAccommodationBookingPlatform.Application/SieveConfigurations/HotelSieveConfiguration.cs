176d87e713d89340781dcd210dfa2188ea33b260
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:39:11 +0300

    feat(sieve): add HotelSieveConfiguration for hotel filtering and sorting

---
+++ TravelAndAccommodationBookingPlatform.Application/SieveConfigurations/HotelSieveConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Sieve.Services;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.SieveConfigurations;
+
+public class HotelSieveConfiguration : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<Hotel>(h => h.Name)
+              .CanFilter()
+              .CanSort();
+
+        mapper.Property<Hotel>(h => h.StarRating)
+              .CanFilter()
+              .CanSort();
+
+        mapper.Property<Hotel>(h => h.Rooms.First().PricePerNight)
+              .HasName("MinRoomPrice")
+              .CanSort();
+    }
+}
