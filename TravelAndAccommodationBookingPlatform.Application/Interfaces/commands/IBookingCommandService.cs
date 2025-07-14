7e12d1c2587993b4c0c217f7e0141690ccd8841c
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 00:57:10 +0300

    feat(bookings): add IBookingCommandService interface for booking command operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/commands/IBookingCommandService.cs
@@ -0,0 +1,9 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Bookings;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Commands;
+
+public interface IBookingCommandService
+{
+    Task<BookingResponseDto> AddBookingAsync(CreateBookingDto createBookingDto);
+    Task DeleteBookingByIdAsync(int bookingId);
+}
