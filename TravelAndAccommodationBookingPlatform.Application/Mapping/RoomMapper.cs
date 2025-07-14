f30aae8079650da07df1b76f3490f962c83fbbac
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 02:04:29 +0300

    feat(rooms): add AutoMapper profile for mapping DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/RoomMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Rooms;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class RoomMapper : Profile
+{
+    public RoomMapper()
+    {
+        CreateMap<CreateRoomDto, Room>();
+        CreateMap<UpdateRoomDto, Room>();
+        CreateMap<Room, RoomDetailsDto>();
+        CreateMap<Room, RoomResponseDto>();
+    }
+}
