eac780a5eaf701d6a217af9c8e49a0833a939623
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:23:24 +0300

    feat(cities): add ICityCommandService interface for city command operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/commands/ICityCommandService.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Cities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Commands;
+
+public interface ICityCommandService
+{
+    Task<AdminCityResponseDto> AddCityAsync(CreateCityDto createCityDto);    
+    Task DeleteCityByIdAsync(int cityId);
+    Task UpdateCityAsync(UpdateCityDto updateCityDto);
+}
