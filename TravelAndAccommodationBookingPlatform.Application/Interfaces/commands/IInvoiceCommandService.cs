ee650fff31f119d74e1dbcc96858941651ef282a
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:44:29 +0300

    feat(invoices): add IInvoiceCommandService interface for invoice command operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/commands/IInvoiceCommandService.cs
@@ -0,0 +1,9 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Invoices;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Commands;
+
+public interface IInvoiceCommandService
+{
+    Task<InvoiceResponseDto> AddInvoiceAsync(CreateInvoiceDto createInvoiceDto);
+    Task UpdateInvoiceAsync(UpdateInvoiceDto updateInvoiceDto);
+}
