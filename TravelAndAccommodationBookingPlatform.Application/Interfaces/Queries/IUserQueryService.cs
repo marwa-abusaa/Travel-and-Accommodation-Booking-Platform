088d949eb88aba92224acc1e97233041990f3b32
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 02:07:26 +0300

    feat(users): add IUserQueryService interface for user read operations

---
+++ TravelAndAccommodationBookingPlatform.Application/Interfaces/Queries/IUserQueryService.cs
@@ -0,0 +1,8 @@
+﻿using TravelAndAccommodationBookingPlatform.Application.Dtos.Users;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Interfaces.Queries;
+
+public interface IUserQueryService
+{
+    Task<TokenResponseDto> LogInAsync(LogInDto logInDto);
+}
