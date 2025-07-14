6cc204485097371df7a390ac5301eb421fd10f6b
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:45:01 +0300

    feat(invoices): add AutoMapper profile for mapping invoice DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/InvoiceMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Invoices;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class InvoiceMapper : Profile
+{
+    public InvoiceMapper()
+    {
+        CreateMap<CreateInvoiceDto, Invoice>();
+        CreateMap<UpdateInvoiceDto, Invoice>();
+        CreateMap<Invoice, InvoiceResponseDto>();
+    }
+}
