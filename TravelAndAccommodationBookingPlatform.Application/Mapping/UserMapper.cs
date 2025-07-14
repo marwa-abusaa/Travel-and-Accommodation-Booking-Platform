4ff01a36beb91d2f50328c0e587dc2f8165a6e1d
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 02:07:43 +0300

    feat(users): add AutoMapper profile for mapping user DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Mapping/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Users;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Mapping;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<SignUpDto, User>();
+        CreateMap<JwtToken, TokenResponseDto>();
+    }
+}
