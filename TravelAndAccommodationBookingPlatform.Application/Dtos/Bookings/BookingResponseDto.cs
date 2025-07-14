184586d57f3952e4cd1e28bdb05761627769222f
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 00:56:29 +0300

    feat(bookings): add Booking DTOs

---
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Bookings/BookingResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Dtos.Bookings;
+
+public class BookingResponseDto
+{
+    public int BookingId { get; set; }
+    public int UserId { get; set; }
+    public List<Room> Rooms { get; set; }
+    public decimal TotalPriceBeforeDiscount { get; set; }
+    public decimal TotalPriceAfterDiscount { get; set; }
+    public string? Remarks { get; set; }
+    public PaymentType PaymentType { get; set; }
+    public DateOnly CheckInDate { get; init; }
+    public DateOnly CheckOutDate { get; init; }
+    public DateOnly BookingDate { get; init; }
+}
