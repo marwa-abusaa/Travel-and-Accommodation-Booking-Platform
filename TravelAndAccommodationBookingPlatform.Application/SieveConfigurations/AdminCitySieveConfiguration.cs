c5ec0dc0351a4fb2f6c7808d4bd41c429d3b8245
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-15 01:30:19 +0300

    feat(sieve): add AdminCitySieveConfiguration for admin city filtering

---
+++ TravelAndAccommodationBookingPlatform.Application/SieveConfigurations/AdminCitySieveConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Sieve.Services;
+using TravelAndAccommodationBookingPlatform.Application.Dtos.Cities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.SieveConfigurations;
+
+public class AdminCitySieveConfiguration : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<AdminCityResponseDto>(x => x.Name).CanFilter().CanSort();
+        mapper.Property<AdminCityResponseDto>(x => x.Country).CanFilter().CanSort();
+        mapper.Property<AdminCityResponseDto>(x => x.PostOffice).CanFilter().CanSort();
+        mapper.Property<AdminCityResponseDto>(x => x.HotelsCount).CanFilter().CanSort();
+        mapper.Property<AdminCityResponseDto>(x => x.CreatedAt).CanSort();
+        mapper.Property<AdminCityResponseDto>(x => x.UpdatedAt).CanSort();
+    }
+}
