19d637d9c8a6d15d92ff9b1c239eed108ac44008
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-20 13:01:45 +0300

    refactor: replace DinkToPdf with NReco.PdfGenerator for PDF generation

--- TravelAndAccommodationBookingPlatform.Application/Services/helpers/PdfGeneratorService.cs
+++ TravelAndAccommodationBookingPlatform.Application/Services/helpers/PdfGeneratorService.cs
@@ -1,32 +1,13 @@
-﻿using DinkToPdf;
-using DinkToPdf.Contracts;
+﻿using NReco.PdfGenerator;
 using TravelAndAccommodationBookingPlatform.Core.Interfaces.Services;
 
 namespace TravelAndAccommodationBookingPlatform.Infrastructure.Services;
 
 public class PdfGeneratorService : IPdfGeneratorService
 {
-    private readonly IConverter _converter;
-
-    public PdfGeneratorService(IConverter converter)
-    {
-        _converter = converter;
-    }
     public byte[] GeneratePdfFromHtml(string htmlContent)
     {
-        var doc = new HtmlToPdfDocument()
-        {
-            GlobalSettings = {
-                PaperSize = PaperKind.A4,
-                Orientation = Orientation.Portrait
-            },
-            Objects = {
-                new ObjectSettings {
-                    HtmlContent = htmlContent
-                }
-            }
-        };
-
-        return _converter.Convert(doc);
+        var converter = new HtmlToPdfConverter();
+        return converter.GeneratePdf(htmlContent);
     }
 }
