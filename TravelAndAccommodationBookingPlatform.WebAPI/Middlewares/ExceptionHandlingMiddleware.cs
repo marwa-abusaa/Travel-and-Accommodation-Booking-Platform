05f82028363d93dac3683ff88857178e7928db80
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-20 13:48:14 +0300

    feat: add custom middleware for centralized exception handling

---
+++ TravelAndAccommodationBookingPlatform.WebAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using TravelAndAccommodationBookingPlatform.WebAPI.Shared;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly GlobalExceptionHandler _exceptionHandler;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, GlobalExceptionHandler exceptionHandler)
+    {
+        _next = next;
+        _exceptionHandler = exceptionHandler;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await _exceptionHandler.TryHandleAsync(context, ex, CancellationToken.None);
+        }
+    }
+}
+
