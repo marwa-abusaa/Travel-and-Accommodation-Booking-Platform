dd83cce2bd05cedc3ec55e9202d1fbfd427e3662
marwa-abusaa <s12028718@stu.najah.edu>
2025-07-21 02:13:58 +0300

    style(dtos): add spacing between properties for readability

--- TravelAndAccommodationBookingPlatform.Application/Dtos/Cities/AdminCityResponseDto.cs
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Cities/AdminCityResponseDto.cs
@@ -6,20 +6,26 @@ public class AdminCityResponseDto
 {
     public int CityId { get; set; }
 
+
     [Sieve(CanFilter = true, CanSort = true)]
     public string Name { get; set; }
+
 
     [Sieve(CanFilter = true, CanSort = true)]
     public string Country { get; set; }
 
+
     [Sieve(CanFilter = true, CanSort = true)]
     public string PostOffice { get; set; }
+
 
     [Sieve(CanFilter = true, CanSort = true)]
     public int HotelsCount { get; set; }
 
+
     [Sieve(CanSort = true)]
     public DateTime CreatedAt { get; set; }
+
 
     [Sieve(CanSort = true)]
     public DateTime? UpdatedAt { get; set; }
--- TravelAndAccommodationBookingPlatform.Application/Dtos/Rooms/AdminRoomResponseDto.cs
+++ TravelAndAccommodationBookingPlatform.Application/Dtos/Rooms/AdminRoomResponseDto.cs
@@ -7,20 +7,26 @@ public class AdminRoomResponseDto
 {
     public int RoomId { get; set; }
 
+
     [Sieve(CanFilter = true)]
     public bool IsAvailable { get; set; }
+
 
     [Sieve(CanFilter = true, CanSort = true)]
     public int AdultCapacity { get; set; }
 
+
     [Sieve(CanFilter = true, CanSort = true)]
     public int ChildrenCapacity { get; set; }
+
 
     [Sieve(CanFilter = true)]
     public RoomClass RoomClass { get; set; }
 
+
     [Sieve(CanSort = true)]
     public DateTime CreatedAt { get; set; }
+
 
     [Sieve(CanSort = true)]
     public DateTime? UpdatedAt { get; set; }
